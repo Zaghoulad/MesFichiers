81c6c213e810e6c63f3794bc5200075c36aa21dc
Zaghoulad <44940960+Zaghoulad@users.noreply.github.com>
2018-11-11 18:00:00 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,18 +5,11 @@ function init() {
         el: '#app',
         data: {
             restaurants: [],
-            input: "",//Pour la recherche
+            input: "",
             page:1,
-            count:0,
             pagesize:10,
             cuisine:"",
-            nom:"",
-            ncuisine:"",//new type de cuisine (pour la modification)
-            nnom:"r",//new nom (pour la modification)
-            //Pour la modification
-            //
-            pid:0//previous id = Ancient id 
-
+            name:""
         },
         mounted() {
             console.log("--- MOUNTED, appelée avant le rendu de la vue ---");
@@ -25,7 +18,7 @@ function init() {
         methods: {
             
             getDataFromWebService: function () {
-                let url = "http://localhost:8080/api/restaurants?page="+this.page+"&name="+this.input+"&pagesize="+this.pagesize;
+                let url = "http://localhost:8080/api/restaurants?pagesize="+this.pagesize+"&name="+this.input+"&page="+this.page;
                 this.input="";
                 fetch(url).then((data) => {
                     console.log("les données sont arrivées !")
@@ -33,12 +26,11 @@ function init() {
                 }).then((dataEnJavaScript) => {
                     // ici on a bien un objet JS
                     this.restaurants = dataEnJavaScript.data;
-                    this.count=parseInt(dataEnJavaScript.count/this.pagesize);
                 });
             },
-            addRestaurant: function () {
+            ajouter: function () {
                 var data= new FormData();
-                data.append("nom",this.nom);
+                data.append("nom",this.name);
                 data.append("cuisine",this.cuisine);
                 fetch("http://localhost:8080/api/restaurants", { 
                 method: "POST", 
@@ -48,28 +40,13 @@ function init() {
                 this.cuisine="";
                 this.getDataFromWebService();
             },
-            modifierRestaurant: function () {
-                var data= new FormData();
-                data.append("nom",this.nnom);
-                data.append("cuisine",this.ncuisine);
-                fetch("http://localhost:8080/api/restaurants/"+this.pid, { 
-                method: "PUT", 
-                body: data 
-                })
-                this.visible.visibility='hidden';
-                this.getDataFromWebService();
-                
-            },
-            supprimerRestaurant: function (restaurant) {
+            supprimer: function (restaurant) {
                 fetch("http://localhost:8080/api/restaurants/"+restaurant._id, { 
                 method: "DELETE", 
                 })
                 this.getDataFromWebService();
             },
-            rendrevisible:function(restaurant){
-                this.nnom=restaurant.name;
-                this.ncuisine=restaurant.cuisine;
-                this.pid=restaurant._id;
+            modifier: function (restaurant) {
                 this.getDataFromWebService();
             },
             getColor: function (index) {
